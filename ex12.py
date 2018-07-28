2e1bfb31733ba7ddbf29e156d92247f2f1b37dbd
wangyiwen82 <1027666542@qq.com>
2018-07-28 04:23:48 -0700

    python homework

---
+++ ex12.py
@@ -0,0 +1,28 @@
+things="apple orange banana grape pear"
+x='a'
+t='b'
+print ''.join(stuff)
+print ','.join(stuff)
+print x in things
+print x not in things
+print x+things
+print 2*things
+print things[1]
+print things[1:10]
+print things[1:14:2]
+print len(things)
+print min(things)
+print max(things)
+print things.index(x)
+print things.count(x)
+print things[5]=t
+print things[1:10]=t
+print del things[2:4]
+print things[1:14:2]=[t]
+print del things[1:14:2]
+print things.append[x]
+print things.clear()
+print things.copy()
+print things.extend(t)
+print things=things*4
+print things.insert(5,x)
